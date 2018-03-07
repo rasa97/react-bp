6227a64f1a0f3ce79a7463791b510f20dca0d5a6
rasa <rasa97uchiha@gmail.com>
2018-03-07 22:01:11 +0530

    Add Render

--- src/app.js
+++ src/app.js
@@ -2,7 +2,11 @@ import React from 'react';
 import { render } from 'react-dom';
 
 class App extends React.Component{
-    
+    render(){
+        return(
+            <p>Hello There!</p>
+        );
+    }
 }
 
 render(<App />, document.getElementById('main'));
